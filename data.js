d34637d147a6bef70676e5752382db9af4e36f76
Stephen Starc <stephenstarc01@gmail.com>
2024-09-05 23:34:33 +0530

    Initial Commit
    All the Initial files are added

---
+++ data.js
@@ -0,0 +1,5 @@
+const data = [
+    { slno: 1, title: 'AWS - Intro', category:'AWS', date: '29 July, 2024', blogLink: 'https://starc-blogs-eac032.webflow.io/posts/aws-introduction'},
+    { slno: 2, title: 'AWS - Cloud Concept', category:'AWS', date: '29 July, 2024', blogLink: 'https://starc-blogs-eac032.webflow.io/posts/cloud-concepts'},
+    // Add more objects as needed
+];
